648b273a7529fef16c1481cf0c08452b0337bb5a
iAaquibjawed <mallickaaquib@gmail.com>
2021-06-08 10:46:31 +0530

    blabla

--- Rails/bill_reminder/app/models/remider.rb
+++
@@ -1,2 +0,0 @@
-class Remider < ApplicationRecord
-end
--- Rails/bill_reminder/db/migrate/20210527053939_create_remiders.rb
+++
@@ -1,8 +0,0 @@
-class CreateRemiders < ActiveRecord::Migration[6.1]
-  def change
-    create_table :remiders do |t|
-
-      t.timestamps
-    end
-  end
-end
